5f803b19c2d4abf19a04725ccddc0cd3eca74983
Overpowered Coding <77810019+OverpoweredCoding@users.noreply.github.com>
2022-01-12 16:05:32 -0700

    Update index.js

--- custom-blockchain/js/index.js
+++ custom-blockchain/js/index.js
@@ -7,7 +7,6 @@ const blockchain = new Blockchain();
 module.exports = async (request, response) => {
     const route = request.url;
 
-    // Keep track of the peers that have contacted us
     blockchain.addPeer(request.headers.host);
 
     let output;
