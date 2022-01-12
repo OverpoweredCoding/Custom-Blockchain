e58f6f54f3a2678810040898c40d432ef8b8ec25
Overpowered Coding <77810019+OverpoweredCoding@users.noreply.github.com>
2022-01-12 16:12:46 -0700

    Update blockchain.py

--- custom-blockchain/blockchain.py
+++ custom-blockchain/blockchain.py
@@ -106,7 +106,6 @@ class Blockchain:
     @staticmethod
     def hash(block):
 
-        # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
